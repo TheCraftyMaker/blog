2e5fea82396397e6541c972a45fab1299d3ba25a
Christof Lauriers <christof.lauriers@gmail.com>
2022-04-08 16:51:00 +0200

    Added blog post detail skeleton component

---
+++ src/SharpSplash.Blog.UI/Pages/BlogPostDetail.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SharpSplash.Blog.UI.Pages
+{
+    public partial class BlogPostDetail
+    {
+        [Parameter] public string Slug { get; set; }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            return;
+        }
+    }
+}

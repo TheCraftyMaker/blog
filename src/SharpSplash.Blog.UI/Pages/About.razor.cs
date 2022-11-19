b9aaf5da0e045356e048f80d5e341091d3a1a3f0
Christof Lauriers <christof.lauriers@gmail.com>
2022-11-19 11:56:16 +0100

    Added about page

---
+++ src/SharpSplash.Blog.UI/Pages/About.razor.cs
@@ -0,0 +1,20 @@
+﻿namespace SharpSplash.Blog.UI.Pages
+{
+    public partial class About
+    {
+        private const string TwitterUser = "krikke21";
+        private const string TwitterUrl = "https://twitter.com/krikke21";
+        private const string MastodonUser = "@thecraftymaker@mastodon.online";
+        private const string MastodonUrl = "https://mastodon.online/@thecraftymaker";
+        private const string YouTubeUser = "The Crafty Maker";
+        private const string YouTubeUrl = "https://www.youtube.com/channel/UCvXzb_8p9glU2eEGPQhUM-A";
+        private const string TikTokUser = "The Crafty Maker (thecraftymakr)";
+        private const string TikTokUrl = "https://www.tiktok.com/@thecraftymakr";
+        private const string InstagramUser = "krikke21";
+        private const string InstagramUrl = "https://www.instagram.com/krikke21/";
+        private const string LinkedInUser = "Christof Lauriers";
+        private const string LinkedInUrl = "https://www.linkedin.com/in/christof-lauriers";
+        private const string SharpSplashUser = "Sharp Splash Blog";
+        private const string SharpSplashUrl = "https://krikke-21.github.io/";
+    }
+}

798c577053ae65de0e60a3bbcd4264511ae26b45
Christof Lauriers <christof.lauriers@euri.com>
2023-03-23 15:08:55 +0100

    Added donation links + cults

--- src/SharpSplash.Blog.UI/Pages/About.razor.cs
+++ src/SharpSplash.Blog.UI/Pages/About.razor.cs
@@ -24,5 +24,11 @@
         private const string PrintablesUrl = "https://www.printables.com/social/283546-thecraftymaker/about";
         private const string ThangsUser = "The Crafty Maker";
         private const string ThangsUrl = "https://thangs.com/designer/TheCraftyMaker";
+        private const string CultsUser = "The Crafty Maker";
+        private const string CultsUrl = "https://cults3d.com/en/users/TheCraftyMaker/creations";
+        private const string BuyMeCoffeeUser = "The Crafty Maker";
+        private const string BuyMeCoffeeUrl = "https://www.buymeacoffee.com/thecraftymaker";
+        private const string PaypalMeUser = "Krikke21";
+        private const string PaypalMeUrl = "https://paypal.me/krikke21";
     }
 }
